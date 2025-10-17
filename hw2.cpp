040e2ca78a51e4b164a9809cdf0f32dde1e79cc9
s1140329-dev <s1140329@dmhs.kh.edu.tw>
2025-10-17 16:24:21 +0800

    Add files via upload

---
+++ hw2.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+#include <string>
+int main() {
+    int age = 99;
+    double height = 2000;
+    std::string name = "Fussen";
+    bool is_pass =true;
+    std::cout << "姓名: "<< name << "身高: "<< height <<std::endl;
+    std::cout << "年齡: "<< age <<"是否及格: " << is_pass << std::endl;
+    return 0;
+    
+}
