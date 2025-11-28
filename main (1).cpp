eb95df88034afbbb12940443ee21eacccc8cd453
s1140329-dev <s1140329@dmhs.kh.edu.tw>
2025-11-28 16:11:36 +0800

    hw8
    hw8

--- main (1).cpp
+++ main (1).cpp
@@ -2,18 +2,12 @@
 using namespace std;
 
 int main() {
-    cout << "請輸入你的年齡:" << endl;
+    int sum = 0;
 
-    int age;
-    cin >> age;
-
-    if (age >= 18) {
-        cout << "你已滿 18 歲，是成年人！" << endl;
-    } else {
-        cout << "你未滿 18 歲，加油努力讀書！" << endl;
+    for (int i = 1; i <= 5; i++) {
+        sum += i;  // 將 i 加到 sum 裡
     }
 
+    cout << "1 + 2 + 3 + 4 + 5 = " << sum << endl;
     return 0;
 }
-
-    
