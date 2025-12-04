4f6e875137043b936bab1ea7da1e6eb987e307c2
s1140329-dev <s1140329@dmhs.kh.edu.tw>
2025-12-04 09:38:12 +0800

    HW9
    HW9

--- main.cpp
+++ main.cpp
@@ -2,29 +2,16 @@
 using namespace std;
 
 int main() {
-    double height, weight, bmi;
-
-    cout << "請輸入身高(cm): ";
-    cin >> height;
-
-    cout << "請輸入體重(kg): ";
-    cin >> weight;
-
-    // 將身高轉換為公尺
-    height = height / 100.0;
-
-    // 計算 BMI
-    bmi = weight / (height * height);
-
-    cout << "您的 BMI = " << bmi << endl;
-
-    
-    if (bmi <18.5) {
-        cout << "你可能過輕" << endl;
-    } else if(bmi<24.9)  {
-        cout << "你屬於正常" << endl;       
-    }else{
-        cout <<"你可能過重";
+    // 外層迴圈表示乘數
+    for (int i = 1; i <= 9; ++i) {
+        // 內層迴圈表示被乘數
+        for (int j = 1; j <= 9; ++j) {
+            // 印出乘法表每個數字
+            cout << i << " * " << j << " = " << i * j << "\t"; 
+        }
+        // 換行
+        cout << endl;
     }
+
     return 0;
 }
