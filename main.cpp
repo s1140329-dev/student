c022fe2b2ca1348f359acac2afdcf5ec92136d91
s1140329-dev <s1140329@dmhs.kh.edu.tw>
2025-12-18 09:45:40 +0800

    hw13
    hw13

--- main.cpp
+++ main.cpp
@@ -2,16 +2,35 @@
 using namespace std;
 
 int main() {
-    // 外層迴圈表示乘數
-    for (int i = 1; i <= 9; ++i) {
-        // 內層迴圈表示被乘數
-        for (int j = 1; j <= 9; ++j) {
-            // 印出乘法表每個數字
-            cout << i << " * " << j << " = " << i * j << "\t"; 
+    // 學生名字
+    string name[5] = {
+        "Amy", "Ben", "Cindy", "David", "Eva"
+    };
+
+    // 學生各科分數 (5個學生，每個學生有3個科目)
+    int score[5][3] = {
+        {80, 90, 85},
+        {70, 75, 78},
+        {88, 92, 95},
+        {60, 65, 70},
+        {90, 93, 89}
+    };
+
+    // 計算每個學生的總分並列出三個科目的分數
+    for (int i = 0; i < 5; ++i) {
+        int total = 0;
+        cout << name[i] << "'s scores: ";
+        
+        // 顯示每個學生的科目分數
+        for (int j = 0; j < 3; ++j) {
+            cout << score[i][j] << " ";
+            total += score[i][j];  // 累加分數
         }
-        // 換行
-        cout << endl;
+        
+        // 顯示學生的總分
+        cout << "Total score: " << total << endl;
     }
 
     return 0;
 }
+\
