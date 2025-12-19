63d8afc93deda00e931c6a803a88adc1670d7c33
s1140329-dev <s1140329@dmhs.kh.edu.tw>
2025-12-19 16:12:54 +0800

    HW14
    HW14

--- main.cpp
+++ main.cpp
@@ -1,36 +1,14 @@
 #include <iostream>
-using namespace std;
 
-int main() {
-    // 學生名字
-    string name[5] = {
-        "Amy", "Ben", "Cindy", "David", "Eva"
-    };
-
-    // 學生各科分數 (5個學生，每個學生有3個科目)
-    int score[5][3] = {
-        {80, 90, 85},
-        {70, 75, 78},
-        {88, 92, 95},
-        {60, 65, 70},
-        {90, 93, 89}
-    };
-
-    // 計算每個學生的總分並列出三個科目的分數
-    for (int i = 0; i < 5; ++i) {
-        int total = 0;
-        cout << name[i] << "'s scores: ";
-        
-        // 顯示每個學生的科目分數
-        for (int j = 0; j < 3; ++j) {
-            cout << score[i][j] << " ";
-            total += score[i][j];  // 累加分數
-        }
-        
-        // 顯示學生的總分
-        cout << "Total score: " << total << endl;
-    }
-
+int main()
+{
+    int score[3]={95,68,42};
+    int temp;
+    temp=score[2];
+    score[2]=score[0];
+    score[0]=temp;
+    for (int i=0;i<3;i++){
+        std::cout<<score[i]<<" ";
+    }   
     return 0;
-}
-\
+ }
